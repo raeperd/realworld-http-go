76bc0cc4e8d2f7e169b393da75a7302bc28ec513
raeperd <raeperd117@gmail.com>
2024-08-10 02:15:28 +0900

    feat: Implements simple post user handler

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/raeperd/realworld"
@@ -12,10 +14,31 @@ func newServer(userService realworld.UserService) http.Handler {
 	return mux
 }
 
-func handlePostUsers(_ realworld.UserService) http.Handler {
+func handlePostUsers(service realworld.UserService) http.Handler {
+	// TODO: handle error
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: implements this
+		req, _ := decode[PostUserRequestBody](r)
+		user := req.User.toUser()
+		user, _ = service.CreateUser(r.Context(), user)
+		_ = encode(w, 201, user)
 	})
+}
+
+func decode[T any](r *http.Request) (T, error) {
+	var v T
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, fmt.Errorf("decode json: %w", err)
+	}
+	return v, nil
+}
+
+func encode[T any](w http.ResponseWriter, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json: %w", err)
+	}
+	return nil
 }
 
 // TODO: Remove this struct by implementing [json.Marshaler]
@@ -31,6 +54,14 @@ type PostUserRequest struct {
 	Name     string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (r PostUserRequest) toUser() realworld.User {
+	return realworld.User{
+		Name:     r.Name,
+		Email:    r.Email,
+		Password: r.Password,
+	}
 }
 
 type PostUserResponse struct {
