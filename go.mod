ef2f63e4cbe2a00def9b44cfbe9c8040d12b597f
raeperd <raeperd117@gmail.com>
2024-08-10 02:06:11 +0900

    test: Add simple testcase for post request

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/raeperd/realworld
 
 go 1.22.5
+
+require github.com/carlmjohnson/requests v0.24.2
+
+require golang.org/x/net v0.27.0 // indirect
---
+++ user.go
@@ -0,0 +1,33 @@
+package realworld
+
+import "context"
+
+type User struct {
+	Name     string
+	Email    string
+	Password string
+}
+
+// TODO: Validate user
+func (u User) Validate() error {
+	return nil
+}
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, user User) (User, error)
+}
+
+type UserService struct {
+	repo UserRepository
+}
+
+func NewUserService(repo UserRepository) UserService {
+	return UserService{repo: repo}
+}
+
+func (u UserService) CreateUser(ctx context.Context, user User) (User, error) {
+	if err := user.Validate(); err != nil {
+		return User{}, err
+	}
+	return u.repo.CreateUser(ctx, user)
+}
