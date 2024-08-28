e67e43c21528a6adc1245f667b02d1e69c300131
raeperd <raeperd117@gmail.com>
2024-08-28 20:53:08 +0900

    refactor: Add route.go for separate route handler

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -1,47 +1,11 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strings"
-	"time"
 
-	"github.com/carlmjohnson/versioninfo"
 	"github.com/raeperd/realworld"
 )
-
-// TODO: refactor this function into new file route.go
-func newServer(userService realworld.UserService, authService realworld.UserAuthService) http.Handler {
-	mux := http.NewServeMux()
-	mux.Handle("GET /health", handleHealthCheck())
-	mux.Handle("POST /api/users", handlePostUsers(userService, authService))
-	mux.Handle("POST /api/users/login", handlePostUsersLogin(authService))
-	mux.Handle("GET /api/user", handleGetUser(authService))
-	mux.Handle("GET /api/profiles/{username}", handleGetProfile(userService))
-	return loggingMiddleware(mux)
-}
-
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		next.ServeHTTP(w, r)
-
-		log.Printf("%s %s %s %s", r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
-	})
-}
-
-var BuildId string
-
-func handleHealthCheck() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_ = encode(w, 200, HealthCheckResponse{
-			BuildId:             BuildId,
-			LastCommitHash:      versioninfo.Revision,
-			LastCommitTimestamp: versioninfo.LastCommit.Unix(),
-		})
-	})
-}
 
 func handlePostUsers(service realworld.UserService, auth realworld.UserAuthService) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
